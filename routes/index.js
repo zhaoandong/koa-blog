8b34dc64b4793801f8a0ea23efa85dfdc7ae8af0
zhaoandong <zhaoandong@hujiang.com>
2016-11-01 11:42:51 +0800

    页面修改

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var router = require('koa-router')();
 
 router.get('/', async function (ctx, next) {
   ctx.state.title = 'koa2 title1';
-  console.log(ctx.state);
   await ctx.render('index');
 })
 module.exports = router;
