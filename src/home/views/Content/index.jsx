cb149561478965c6ac0766163acb45662721503a
zhaoandong <zhaoandong@hujiang.com>
2017-01-08 22:56:00 +0800

    add: react-markdown

--- src/home/views/Content/index.jsx
+++ src/home/views/Content/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 import './index.scss'
 import Header from '../../components/header/header.jsx'
 import Sidebar from '../../components/Sidebar/index.jsx'
 import Content from '../../components/content/content.jsx'
 import HomeTitle from '../../components/HomeTitle/index.jsx'
 
+
+var input = '# This is a header\n\nAnd this is a paragraph\n\n# This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph  # This is a header\n\nAnd this is a paragraph';
 
 
 class Home extends React.Component {
@@ -17,7 +20,7 @@ class Home extends React.Component {
 				        	<Header />
 				          <div className="ant-layout-container">
 				            <div className="ant-layout-content">
-				            	我是正文
+				            	<ReactMarkdown source={input} />
 				            </div>
 				          </div>
 				        </div>
