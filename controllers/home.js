5bfadf9c4e8cadf7b48571f1a49c9255581dfcb5
zhaoandong <zhaoandong@hujiang.com>
2017-01-13 23:49:49 +0800

    Revert "add: bug暂存"
    This reverts commit cccc16ccda57a422e26e625a21dca27ac8c5de78.

--- controllers/home.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-    index: function* (scope) {  
-    	scope.title = 'koa2 title1';
-    	console.log(1221)
-	    yield this.render("index");
-
-    }
-}
--- route.js
+++
@@ -1,7 +0,0 @@
-var router = require("./lib/router"),
-    get = router.get,
-    post = router.post,
-    put = router.put,
-    all = router.all;
-
-get("/", "home.index");
