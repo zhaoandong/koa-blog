ac1202d4f095b7aea0a0571741beff615e795523
zhaoandong <zhaoandong@hujiang.com>
2017-02-20 22:35:14 +0800

    add:markdown

--- src/home/views/Content/index.jsx
+++ src/home/views/Content/index.jsx
@@ -9,14 +9,14 @@ import { Spin } from 'antd'
 
 
 //var input = '# This is a header\n\nAnd this is a paragraph\n\n# This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph  # This is a header\n\nAnd this is a paragraph';
-let test ='# This is a header\n\nAnd this is a paragraph\n\n# This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph  # This is a header\n\nAnd this is a paragraph'
+//let test ='# This is a header\n\nAnd this is a paragraph\n\n# This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph # This is a header\n\nAnd this is a paragraph  # This is a header\n\nAnd this is a paragraph'
 
 class Home extends React.Component {
 	constructor(props) {
 		
 		super(props);
 		this.state = {
-			data: ""
+			data: ''
 		};
 
 	}	
@@ -27,16 +27,17 @@ class Home extends React.Component {
 
 		fetch("api/content").then(function(res) {
 		
-			console.log(res)
 			if (res.ok) {
 				res.json().then(function(data) {
-					console.log(data)
+
 					self.setState({
-						data: data
+						data: data.content
 					})
 				});
 			} else {
+
 				console.log("Looks like the response wasn't perfect, got status", res.status);
+			
 			}
 		
 		}, function(e) {
@@ -52,27 +53,26 @@ class Home extends React.Component {
 
 	}
 
-    render() {
+  render() {
+
 
 		let mainComopent = this.state.data ? 	<ReactMarkdown source={this.state.data} /> : <Spin size="large" /> ;
 
-        return (
-            <div className="ant-layout-aside">
-                <Sidebar />
-			        <div className="ant-layout-main">
-			        	<Header />
-			          <div className="ant-layout-container">
-			            <div className="ant-layout-content">
+    return (
+        <div className="ant-layout-aside">
+            <Sidebar />
+	        <div className="ant-layout-main">
+	        	<Header />
+	          <div className="ant-layout-container">
+	            <div className="ant-layout-content">
 
-			            	{mainComopent}
-			            	
-			            </div>
-			          </div>
-			        </div>
-            </div>
-        );
-
-    	// } 
+	            	{mainComopent}
+	            	
+	            </div>
+	          </div>
+	        </div>
+        </div>
+    );
 
     }
 }
